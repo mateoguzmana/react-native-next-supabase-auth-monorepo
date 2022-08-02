135a65ab1114b413407fb6c7ed194edf39194c83
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2022-08-02 22:15:43 +0200

    Installing async storage and configuring supabase to use it

--- packages/app/src/utils/supabase-client.ts
+++ packages/app/src/utils/supabase-client.ts
@@ -1,6 +1,13 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL as string ?? '',
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string ?? ''
-);
+const supabaseUrl = (process.env.NEXT_PUBLIC_SUPABASE_URL as string) ?? '';
+const supabaseAnonKey =
+  (process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string) ?? '';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  localStorage: AsyncStorage as any,
+  autoRefreshToken: true,
+  persistSession: true,
+  detectSessionInUrl: false
+});
