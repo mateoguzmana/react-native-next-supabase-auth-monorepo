3d23306de8ad5dcdab6832874c48daa741fdbc46
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2022-08-04 21:26:28 +0200

    Cleaning up non-needed logic.

--- packages/next/utils/image-picker.ts
+++ packages/next/utils/image-picker.ts
@@ -77,113 +77,14 @@ const DEFAULT_OPTIONS: ImageLibraryOptions & CameraOptions = {
 };
 
 export const imagePicker = async () => {
-  // const result = await openFileBrowserAsync({
-  //   mediaTypes: 'jpg',
-  //   allowsMultipleSelection: true,
-  //   capture: true,
-  //   base64: true
-  // });
+  const result = await launchImageLibrary({
+    selectionLimit: 1,
+    mediaType: 'photo',
+    includeBase64: true
+  });
 
-  // return result;
-
-  const result = await launchImageLibrary({ selectionLimit: 1, mediaType: 'photo', includeBase64: true });
-
-  return result
+  return result;
 };
-
-function openFileBrowserAsync({
-  mediaTypes,
-  capture = false,
-  allowsMultipleSelection = false,
-  base64
-}) {
-  const mediaTypeFormat = mediaTypes;
-
-  const input = document.createElement('input');
-  input.style.display = 'none';
-  input.setAttribute('type', 'file');
-  input.setAttribute('accept', mediaTypeFormat);
-  input.setAttribute('id', 'whateverid');
-  if (allowsMultipleSelection) {
-    input.setAttribute('multiple', 'multiple');
-  }
-  if (capture) {
-    input.setAttribute('capture', 'camera');
-  }
-  document.body.appendChild(input);
-
-  return new Promise(resolve => {
-    input.addEventListener('change', async () => {
-      if (input.files) {
-        if (!allowsMultipleSelection) {
-          const img = await readFile(input.files[0], { base64 });
-          console.log({ img })
-          resolve({ ...(img as any) });
-        } else {
-          const imgs = await Promise.all(
-            Array.from(input.files).map(file => readFile(file, { base64 }))
-          );
-
-          console.log({ imgs })
-          resolve({
-            cancelled: false,
-            selected: imgs
-          });
-        }
-      }
-      document.body.removeChild(input);
-    });
-
-    const event = new MouseEvent('click');
-    input.dispatchEvent(event);
-  });
-}
-
-function readFile(targetFile: Blob, options: { base64: boolean }) {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onerror = () => {
-      reject(
-        new Error(
-          `Failed to read the selected media because the operation failed.`
-        )
-      );
-    };
-    reader.onload = ({ target }) => {
-      const uri = target.result;
-      const returnRaw = () =>
-        resolve({
-          uri,
-          width: 0,
-          height: 0,
-          cancelled: false
-        });
-
-      if (typeof uri === 'string') {
-        const image = new Image();
-        image.src = uri;
-        image.onload = () =>
-          resolve({
-            uri,
-            width: image.naturalWidth ?? image.width,
-            height: image.naturalHeight ?? image.height,
-            cancelled: false,
-            // The blob's result cannot be directly decoded as Base64 without
-            // first removing the Data-URL declaration preceding the
-            // Base64-encoded data. To retrieve only the Base64 encoded string,
-            // first remove data:*/*;base64, from the result.
-            // https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsDataURL
-            ...(options.base64 && { base64: uri.substr(uri.indexOf(',') + 1) })
-          });
-        image.onerror = () => returnRaw();
-      } else {
-        returnRaw();
-      }
-    };
-
-    reader.readAsDataURL(targetFile);
-  });
-}
 
 export function launchCamera(
   _: ImageLibraryOptions,
@@ -235,14 +136,18 @@ export function launchImageLibrary(
       if (input.files) {
         if (options.selectionLimit <= 1) {
           const img = await readFileForRNWeb(input.files[0], {
-            base64: options.includeBase64
+            includeBase64: options.includeBase64
           });
 
-          resolve({ assets: [img] });
+          const result = { assets: [img] }
+
+          if (callback) callback(result);
+
+          resolve(result);
         } else {
           const imgs = await Promise.all(
             Array.from(input.files).map(file =>
-              readFileForRNWeb(file, { base64: options.includeBase64 })
+              readFileForRNWeb(file, { includeBase64: options.includeBase64 })
             )
           );
 
@@ -264,7 +169,10 @@ export function launchImageLibrary(
   });
 }
 
-function readFileForRNWeb(targetFile: Blob, options: { base64: boolean }): Promise<Asset> {
+function readFileForRNWeb(
+  targetFile: Blob,
+  options: Partial<ImageLibraryOptions>
+): Promise<Asset> {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
     reader.onerror = () => {
@@ -276,11 +184,12 @@ function readFileForRNWeb(targetFile: Blob, options: { base64: boolean }): Promi
     };
     reader.onload = ({ target }) => {
       const uri = target.result;
+
       const returnRaw = () =>
         resolve({
           uri: uri as string,
           width: 0,
-          height: 0,
+          height: 0
         });
 
       if (typeof uri === 'string') {
@@ -296,7 +205,9 @@ function readFileForRNWeb(targetFile: Blob, options: { base64: boolean }): Promi
             // Base64-encoded data. To retrieve only the Base64 encoded string,
             // first remove data:*/*;base64, from the result.
             // https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsDataURL
-            ...(options.base64 && { base64: uri.substr(uri.indexOf(',') + 1) })
+            ...(options.includeBase64 && {
+              base64: uri.substr(uri.indexOf(',') + 1)
+            })
           });
         image.onerror = () => returnRaw();
       } else {
